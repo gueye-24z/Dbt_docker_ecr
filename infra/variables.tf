de57374ebc30a593b2db9238c61f7e14d1422986
Gueye <mohamed.ge21@gmail.com>
2024-11-20 11:57:59 +0100

    feat: ajout des variables Terraform

---
+++ infra/variables.tf
@@ -0,0 +1,39 @@
+variable "SNOWFLAKE_DATABASE" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_ACCOUNT" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_SCHEMA" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_USER" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_WAREHOUSE" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_ROLE" {
+    type = string
+    description
+}
+
+variable "SNOWFLAKE_PASSWORD" {
+    type = string
+    description
+}
+
+variable "TASK_NAME" {
+    type = string
+    description
+}
