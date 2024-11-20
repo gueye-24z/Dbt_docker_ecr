d5aefa225e5c2a2022bd8ae31484f0f2aa4a5010
Gueye <mohamed.ge21@gmail.com>
2024-11-20 12:12:31 +0100

    fix: modifs descriptions pour les variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,39 +1,39 @@
 variable "SNOWFLAKE_DATABASE" {
     type = string
-    description = Base de données de travail
+    description = "Base de données de travail"
 }
 
 variable "SNOWFLAKE_ACCOUNT" {
     type = string
-    description = Compte snowflake
+    description = "Compte snowflake"
 }
 
 variable "SNOWFLAKE_SCHEMA" {
     type = string
-    description = Schéma snowflake contenant les tables
+    description = "Schéma snowflake contenant les tables"
 }
 
 variable "SNOWFLAKE_USER" {
     type = string
-    description = Utilisateur snowflake
+    description = "Utilisateur snowflake"
 }
 
 variable "SNOWFLAKE_WAREHOUSE" {
     type = string
-    description = Warehouse snowflake
+    description = "Warehouse snowflake"
 }
 
 variable "SNOWFLAKE_ROLE" {
     type = string
-    description = Role snowflake
+    description = "Role snowflake"
 }
 
 variable "SNOWFLAKE_PASSWORD" {
     type = string
-    description = Mot de passe snowflake
+    description = "Mot de passe snowflake"
 }
 
 variable "TASK_NAME" {
     type = string
-    description = Nom de la tache
+    description = "Nom de la tache"
 }
